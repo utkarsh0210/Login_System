55b0497968664f145e01b5d98c98ceaf2e4c2954
utkarsh0210 <116905545+utkarsh0210@users.noreply.github.com>
2023-07-12 21:37:36 +0530

    Update urls.py

--- Login/urls.py
+++ Login/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from login.views import packet_list
 
 urlpatterns = [
     path ('', views.home,name="home"),
     path ('signUp',views.signUp, name="signUp"),
     path ('signIn',views.signIn, name="signIn"),
     path ('signOut',views.signOut, name="signOut"),
+    path('packets/', packet_list, name='packet_list'),
 ]
